ddd79fd531472a02f25517d02b067e07e155361c
= <aryansahoo2021@gmail.com>
2023-04-18 01:34:30 +0530

    f

--- stud_profile.php
+++ stud_profile.php
@@ -63,7 +63,16 @@ a:hover {
 				$sql = "SELECT * FROM Student WHERE Email='$email'";
 				$result = mysqli_query($conn, $sql);
 				if (mysqli_num_rows($result) == 1) {
-					$student = mysqli_fetch_assoc($result);
+                    $student = mysqli_fetch_assoc($result);
+                    // $batch_year = int(substr($student['RollNo'],0,2));
+                    //Retrieve current year
+                    $current_year = date("Y");
+                    $alpha = "20". substr($student['RollNo'], 0, 2);
+                    //Retrieve first two characters of RollNo from $student array and convert to integer
+                    $rollno_year = intval($alpha);
+                    //Calculate difference between $rollno_year and $current_year
+                    $year_diff = $current_year - $rollno_year+1;
+                    //Display year difference as string
 					echo "<h1>Welcome, ".$student['Name']."</h1>";
 					echo "<table>";
 					echo "<tr><th>RollNo</th><td>".$student['RollNo']."</td></tr>";
@@ -72,6 +81,14 @@ a:hover {
 					echo "<tr><th>12th</th><td>".$student['12th']."</td></tr>";
 					echo "<tr><th>CPI</th><td>".$student['CPI']."</td></tr>";
 					echo "<tr><th>DOB</th><td>".$student['DOB']."</td></tr>";
+                    if($year_diff==1){echo "<tr><th>Batch Year</th><td>".$year_diff."st year</td></tr>";}
+                    else if($year_diff==2)
+                    {echo "<tr><th>Batch Year</th><td>".$year_diff."nd year</td></tr>";}
+                    else if($year_diff==3)
+                    {echo "<tr><th>Batch Year</th><td>".$year_diff."rd year</td></tr>";}
+                    else if($year_diff==4)
+                    {echo "<tr><th>Batch Year</th><td>".$year_diff."th year</td></tr>";}
+                    // echo "<tr><th>Batch Year</th><td>".$year_diff."st/nd/rd/th year</td></tr>";
 					echo "<tr><th>Specialization</th><td>".$student['Specialization']."</td></tr>";
 					echo "<tr><th>Area of Interest</th><td>".$student['Area_of_Interest']."</td></tr>";
 					echo "<tr><th>File Link</th><td><a href='".$student['file_link']."'>Download</a></td></tr>";
